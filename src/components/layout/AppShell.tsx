c17d062b554cafe8583702f4c119b2c562e109ed
PRIYANSHU MAURYA <159282128+XIZOAHUB@users.noreply.github.com>
2026-08-11 15:13:48 +0530

    Update AppShell.tsx

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -1,18 +1,17 @@
-import { Outlet, Link, ... } from "react-router-dom";
-import { Outlet } from "react-router-dom";   // duplicate
-import Sidebar from './Sidebar'
-import TopBar from './TopBar'
+import { Outlet } from "react-router-dom";
+import Sidebar from './Sidebar';
+import TopBar from './TopBar';
 
-export default function AppShell({ children }: { children: React.ReactNode }) {
+export default function AppShell() {
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-dark-950">
       <TopBar />
       <div className="flex">
         <Sidebar />
         <main className="flex-1 ml-64 p-6 pt-20">
-          {children}
+          <Outlet />
         </main>
       </div>
     </div>
-  )
+  );
 }
